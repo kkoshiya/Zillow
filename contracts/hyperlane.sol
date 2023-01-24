da4c68038d0c2249b9d4c83973278fe555f5f492
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2023-01-23 20:44:38 -0800

    hyperlane routers

--- contracts/hyperlane.sol
+++ contracts/hyperlane.sol
@@ -67,8 +67,6 @@ contract HelloWorld is Router {
 
     // ============ Internal functions ============
 
-    
-
     /**
      * @notice Handles a message from a remote router.
      * @dev Only called for messages sent from a remote router, as enforced by Router.sol.
